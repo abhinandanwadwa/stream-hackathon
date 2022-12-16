50ad3b9797bca8630f7b90589aae4de7794617d1
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2022-12-16 18:17:27 +0530

    Added the Blog section and connected with the backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="/" element={<Blogs />} />
       </Routes>
     </BrowserRouter>
     </div>
