0f293766e7927d31d6292d8f1ef815d3664829c4
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2022-12-16 10:14:32 +0530

    Setting up React router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
